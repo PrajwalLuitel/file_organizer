c4087bd6090a215cf342e67097df2c7ac6b64c8c
PrajwalLuitel <prajwalluitel@gmai.com>
2021-10-29 13:32:23 +0545

    initial development completed

---
+++ main.py
@@ -0,0 +1,74 @@
+import shutil
+import os
+from os import listdir
+from os.path import isfile, join
+from location import dir_location, project_location
+
+# The path of the directory which is to be organized
+location = dir_location
+
+
+def classify(filename, foldername):
+    f = os.listdir(location)
+    if foldername[:-1] not in f:
+        os.mkdir(location+foldername)
+
+    shutil.move(location+filename, location+foldername)
+
+
+def organize_files(location):
+    files = [f for f in listdir(location) if isfile(join(location,f))]
+    for file in files:
+        extension = file.split(".")[-1]
+
+        if extension == "pdf":
+            classify(file, "pdf_files/")
+
+        elif extension in ["pptx", "ppt", "pptm", "potx", "potm", "ppsx", "ppsm"]:
+            classify(file, "presentation_files/")
+
+        elif extension in ["docx", "dox", "doc"]:
+            classify(file, "word_files/")
+
+        elif extension in ["jpg", "png", "jpeg", "raw", "psd", "gif"]:
+            classify(file, "images_and_gifs/")
+
+        elif extension in ["mp4", "mpg", "mp2", "mpeg", "mpe", "mpv", "m4p", "m4v", "avi", "wmv", "mov", "qt", "flv", "swf"]:
+            classify(file, "videos_and_movies/")
+
+        elif extension in ["mp3", "aac", "flac", "alac", "wav", "aiff", "dsd", "pcm", "mid"]:
+            classify(file, "audios_and_songs/")
+
+        elif extension in ["xls", "xlr", "xlsx", "xlsb", "xml", "csv"]:
+            classify(file, "spreadsheet_files/")
+
+        elif extension in ["txt"]:
+            classify(file, "text_files/")
+
+        elif extension in ["html", "css", "js", "php"]:
+            classify(file, "web_files/")
+
+        elif extension in ["gz", "rar", "7z", "rpm", "zip", "zipx"]:
+            classify(file, "compressed_files/")
+
+        elif extension in ["vcd", "iso", "mdf", "bin"]:
+            classify(file, "disk_image_files/")
+
+        elif extension in ["c", "py", "java", "cpp"]:
+            classify(file, "developer_files/")
+            if extension == "c":
+                classify("developer_files/"+file, "developer_files/C_files/")
+            elif extension == "py":
+                classify("developer_files/"+file, "developer_files/Python_files/")
+            elif extension == "java":
+                classify("developer_files/"+file, "developer_files/Java_files/")
+            elif extension == "cpp":
+                classify("developer_files/"+file, "developer_files/CPP_files/")
+
+
+if __name__ == "__main__":
+    try:
+        while(True):
+            organize_files(location)
+    except Exception as e:
+        os.system(f'cmd /k "pythonw {project_location}main.py"') #The project_location is the location of the file main.py
---
+++ readme.md
@@ -0,0 +1,12 @@
+This project arranges the files present into different folders according to the file extensions. 
+
+How to run:
+
+In 
+line 8: location = dir_location 
+(replace the dir_location with the location of directory you wish to arrange)
+
+In 
+line 74: os.system(f'cmd /k "pythonw {project_location}main.py"')
+replace the {project_location} with the location of the main.py file in your system
+
